ed3629ff62a82ecd6bcdfdfb63de519bdcd48785
osatashamim <shamimosata@gmail.com>
2023-10-05 12:19:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -use puts to print a line
+ * Return: 0 (good to go)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
