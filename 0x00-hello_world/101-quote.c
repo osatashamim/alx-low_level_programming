fd46fa09b2c21f9611aecc776832df62dc801ec0
osatashamim <shamimosata@gmail.com>
2023-10-05 13:58:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - a program that prints a line to standard error without using printf or puts
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
