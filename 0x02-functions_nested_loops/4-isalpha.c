98d760231c0c3fb63435c990be4a05444f9da885
osatashamim <shamimosata@gmail.com>
2023-10-12 15:00:14 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - function checks for lowercase and uppercase alphabets
+ * @c: parameter to be checked
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
